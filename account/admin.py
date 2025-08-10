f488e71c4fb54b9147feaa35ff62b5c0148c1418
Rishe <rishe3518@gmail.com>
2025-08-10 11:10:52 +0530

    17857

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Menu,Order
 
-# Register your models here.
+class MenuAdmin(admin.ModelAdmin):
+    list_display=('name','price','available')
+    search_fields=('name',)
+    list_filter=('available',)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display=('customer_name','menu_item','quantity','status')
+    search_fields=('customer_name',)
+    list_filter=('status',)
