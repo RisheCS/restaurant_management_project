72afa6d64803a7a02466ed60b7b2c2639476f36b
Rishe <rishe3518@gmail.com>
2025-08-24 17:37:24 +0530

    37866

--- home/views.py
+++ home/views.py
@@ -3,3 +3,8 @@ from .models import Restaurant
 
 def about_view(request):
     return render(request, 'about.html')
+def home_view(request):
+    context = {
+        "restaurant_name": "Our Restaurant"
+    }
+    return render(request, "home.html",context)
